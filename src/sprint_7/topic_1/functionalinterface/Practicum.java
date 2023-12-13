74da788a2c9ede43ce1bb92fc99a1ced6fa0a0a4
Stanislav <msvdev@yandex.com>
2023-12-14 01:01:56 +0500

    feature: add code with lambda

---
+++ src/sprint_7/topic_1/functionalinterface/Practicum.java
@@ -0,0 +1,17 @@
+package sprint_7.topic_1.functionalinterface;
+
+@FunctionalInterface
+interface Summator {
+    int sum(int n1, int n2);
+}
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        // сохраните лямбду в переменную
+        // лямбда должна складывать два целых числа
+        Summator summator = (n1, n2) -> n1 + n2;
+        // допишите код, использующий лямбду
+        System.out.println("Сумма 23 и 65 равна " + summator.sum(23, 65));
+    }
+}
