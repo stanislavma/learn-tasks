0b232d7357ccf5518c40fea467df9232305797e9
Stanislav <msvdev@yandex.com>
2023-12-15 01:39:42 +0500

    feature: work with stream api and complete task with candies

---
+++ src/sprint_7/topic_1/optional/task2/Candy.java
@@ -0,0 +1,30 @@
+package sprint_7.topic_1.optional.task2;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Candy {
+    // название
+    final String name;
+    // цена
+    final double price;
+    //проданное количество
+    final int amount;
+    // другие варианты названия
+    final Set<String> alternativeNames;
+
+    public Candy(String name, double price, int amount, Collection<String> alternativeNames) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.alternativeNames = Set.copyOf(alternativeNames);
+    }
+
+    public Candy(String name, double price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.alternativeNames = new HashSet<>();
+    }
+}
---
+++ src/sprint_7/topic_1/optional/task2/SearchService.java
@@ -0,0 +1,51 @@
+package sprint_7.topic_1.optional.task2;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Optional;
+
+public class SearchService {
+    // Создаём объект класса, отвечающий за склад магазина
+    private final Warehouse warehouse = new Warehouse();
+    // Создаём объект класса, отвечающий за работу с поставщиками
+    public final SRM srm = new SRM();
+
+    // Основной метод поиска
+    // Проверяет наличие товара с указанным именем на складе магазина
+    // Если товар отсутствует, то проверяются склады поставщиков,
+    // предпочтение отдаётся тому поставщику, у которого наименьшая цена товара.
+    // Для поиска товара на складе поставщиков используется метод supplierSearch
+    // Если товар нигде не найден, то возвращается пустой Optional
+    public Optional<Candy> search(String candyName) {
+        Optional<Candy> candy = Optional.empty();
+
+        if (!candyName.isEmpty()) {
+            candy = warehouse.search(candyName);
+
+            if (candy.isEmpty()) {
+                candy = supplierSearch(candyName);
+            }
+
+        } else {
+            candy = Optional.empty();
+        }
+
+        return candy;
+
+    }
+
+    // Ищет товар с указанным именем на складах поставщиков
+    // Возвращает Optional с самым дешевым вариантом товара среди всех
+    // поставщиков или пустой Optional, если товар не найден
+    private Optional<Candy> supplierSearch(String candyName) {
+        // Реализуйте данный метод при помощи Stream API и Optional,
+        // используйте метод min из Stream API для нахождения товара с наименьшей ценой
+
+        Optional<Candy> findCandy = srm.listSuppliers().stream()
+                .map(supplier -> srm.getProduct(supplier, candyName)) // Получаем конфету у каждого поставщика
+                .filter(Objects::nonNull) // Исключаем null значения (т.е. конфеты, которые не найдены)
+                .max(Comparator.comparing(candy -> candy.price));
+
+        return findCandy;
+    }
+}
