cc088473839e1aaa5ac7b0c5eed9a71a563e9fba
stanislavma <msvdev@yandex.com>
2024-02-02 22:43:53 +0500

    feature: add conversion from api

---
+++ src/sprint_8/topic_7/lesson_3/task_1/Practicum.java
@@ -0,0 +1,29 @@
+package sprint_8.topic_7.lesson_3.task_1;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        // делаем запрос с параметром name
+        URI url = URI.create("https://api.agify.io/?name=Pixel");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Код состояния: " + response.statusCode());
+            // в ответе будет содержаться предсказанный сервисом возраст
+            System.out.println("Ответ: " + response.body());
+        } catch (IOException | InterruptedException e) { // обрабатываем ошибки отправки запроса
+            System.out.println("Во время выполнения запроса ресурса по url-адресу: '" + url + "', возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+}
---
+++ src/sprint_8/topic_7/lesson_4/TaskThree.java
@@ -0,0 +1,49 @@
+package sprint_8.topic_7.lesson_4;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        URI url = URI.create("https://functions.yandexcloud.net/d4ed1i6t3f80hf0p7mer?base=RUB&symbols=USD,EUR");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // проверяем, успешно ли обработан запрос
+            if (response.statusCode() == 200) {
+                JsonElement jsonElement = JsonParser.parseString(response.body());
+                if(!jsonElement.isJsonObject()) { // проверяем, точно ли мы получили JSON-объект
+                    System.out.println("Ответ от сервера не соответствует ожидаемому.");
+                    return;
+                }
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+                JsonObject rates = jsonObject.get("rates").getAsJsonObject();
+
+                // получите курс доллара и евро и запишите в переменные rateUSD и rateEUR
+               double rateUSD = rates.get("USD").getAsDouble();
+               double rateEUR  = rates.get("EUR").getAsDouble();
+
+                System.out.println("Стоимость рубля в долларах: " + rateUSD + " USD");
+                System.out.println("Стоимость рубля в евро: " + rateEUR + " EUR");
+            } else {
+                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) { // обрабатываем ошибки отправки запроса
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+}
